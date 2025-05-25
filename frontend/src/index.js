df822fc870184d07fb7c824b7768414bfece5a56
Rubinschwein47 <felixconrad7@gmail.com>
2025-05-25 03:15:28 +0200

    stable and can start

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import openapiTS from "openapi-typescript";
+// import openapiTS from "openapi-typescript";
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
