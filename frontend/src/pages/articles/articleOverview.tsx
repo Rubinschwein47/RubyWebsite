f6bf8fb005ab9facfcdef5d43983ede7999e6c82
rubinchwein <felixconrad7@gmail.com>
2026-01-26 23:01:34 +0100

    minor frontend progre article

--- frontend/src/pages/articles/articleOverview.tsx
+++ frontend/src/pages/articles/articleOverview.tsx
@@ -1,10 +1,17 @@
 import React from "react";
-import {Typography} from "antd";
+import {Input, Typography} from "antd";
+import Trans from "../../basics/Translate";
+import "./articleOverview.css";
 
 const {Text,Title} = Typography;
 
 export default function ArticleOverview(){
     return (<>
-        <Title>Artikel funkt</Title>
+        <Title level={1}><Trans path={"article.header"}/></Title>
+        <div className={"container article-search-container"}>
+            <span style={{width:'20rem'}}><Input placeholder={"article.search.title"}/></span>
+            <span style={{width:'5rem'}}><Input placeholder={"Title"}/></span>
+            
+        </div>
     </>);
 }
